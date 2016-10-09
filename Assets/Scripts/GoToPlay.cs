5d98bc7a2daca9704e1590cce8a1d72e2ebf6589
ttanatb <ttanatb@gmail.com>
2016-10-09 14:41:47 -0400

    Added more screens

---
+++ Assets/Scripts/GoToPlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToPlay : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("J1A") || Input.GetButtonDown("J2A"))
+        {
+            Application.LoadLevel("MainGame");
+        }
+    }
+
+    void OnClick()
+    {
+       
+    }
+}
